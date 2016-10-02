f321e77ba22107b18272949d8e207695755b2b37
Ricardo Bravo <ricardobravo@outlook.com>
2016-10-02 16:36:57 -0500

    chore(init): initial commit

---
+++ app/src/main/java/com/rba/pushnotification/Food.java
@@ -0,0 +1,27 @@
+package com.rba.pushnotification;
+
+/**
+ * Created by Ricardo Bravo on 1/10/16.
+ */
+
+public class Food {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
